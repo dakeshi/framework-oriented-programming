a577a61b437edc7ff9e7c595142e3a240c6ddcd6
Pedro Piñera Buendía <pepibumur@gmail.com>
2016-04-18 08:44:51 +0200

    Create frameworks

---
+++ FCore/FCore/FCore.h
@@ -0,0 +1,19 @@
+//
+//  FCore.h
+//  FCore
+//
+//  Created by Pedro Pinera Buendia on 18/04/16.
+//  Copyright © 2016 com.fop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FCore.
+FOUNDATION_EXPORT double FCoreVersionNumber;
+
+//! Project version string for FCore.
+FOUNDATION_EXPORT const unsigned char FCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FCore/PublicHeader.h>
+
+
---
+++ FPersistency/FPersistency/FPersistency.h
@@ -0,0 +1,19 @@
+//
+//  FPersistency.h
+//  FPersistency
+//
+//  Created by Pedro Pinera Buendia on 18/04/16.
+//  Copyright © 2016 com.fop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FPersistency.
+FOUNDATION_EXPORT double FPersistencyVersionNumber;
+
+//! Project version string for FPersistency.
+FOUNDATION_EXPORT const unsigned char FPersistencyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FPersistency/PublicHeader.h>
+
+
