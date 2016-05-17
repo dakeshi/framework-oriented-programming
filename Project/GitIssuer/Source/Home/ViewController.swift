25960074e8aa643890fa114c2cc9d2c86e999656
Pedro Piñera Buendía <pepibumur@gmail.com>
2016-05-17 09:53:24 +0200

    Recreated project from the scratch

---
+++ Project/GitIssuer/Source/Home/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitIssuer
+//
+//  Created by Pedro Pinera Buendia on 17/05/16.
+//  Copyright © 2016 es.ppinera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
